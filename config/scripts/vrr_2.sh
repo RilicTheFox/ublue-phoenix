559e20eb7a9350afb571086fb031e31a606c82ad
RilicTheFox <RilicTheFox@users.noreply.github.com>
2023-11-03 10:39:46 +0000

    Add VRR another way (thanks fiftydinar!)

--- config/scripts/vrr_2.sh
+++ config/scripts/vrr_2.sh
@@ -2,4 +2,4 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-echo "experimental-features=['variable-refresh-rate']" >> /usr/etc/dconf/db/local.d/01-phoenix
+echo "experimental-features=['variable-refresh-rate']" >> /usr/etc/dconf/db/local.d/01-gidro
