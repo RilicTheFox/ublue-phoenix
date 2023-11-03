a5aaa755eebbc04f8c6ef0c26ad565c6fda72fab
RilicTheFox <RilicTheFox@users.noreply.github.com>
2023-11-03 10:50:29 +0000

    Add some default dconf settings

--- config/scripts/vrr_2.sh
+++ config/scripts/vrr_2.sh
@@ -2,4 +2,4 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-echo "experimental-features=['variable-refresh-rate']" >> /usr/etc/dconf/db/local.d/01-gidro
+echo "experimental-features=['variable-refresh-rate']" >> /usr/etc/dconf/db/local.d/01-phoenix
